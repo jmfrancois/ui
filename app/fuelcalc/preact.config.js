6be1236538267e057458264f887ea99546fc1f4f
Jean-Michel FRANCOIS <jeanmichel.francois@gmail.com>
2024-01-19 23:32:37 +0100

    chore: import

---
+++ app/fuelcalc/preact.config.js
@@ -0,0 +1,10 @@
+export default (config) => {
+	if (process.env.NODE_ENV !== "development") {
+		console.log("### /fuelcalc dir");
+		config.output.publicPath = "/fuelcalc";
+	} else {
+		console.log("### / dir");
+		config.output.publicPath = "/";
+	}
+	return config;
+};
