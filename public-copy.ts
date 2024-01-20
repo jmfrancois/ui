3e4fe0d72f2f2e7ad8cf41c1351e6a3e8223e936
Jean-Michel FRANCOIS <jeanmichel.francois@gmail.com>
2024-01-20 23:23:21 +0100

    chore: add home page

--- public-copy.ts
+++ public-copy.ts
@@ -1,13 +1,10 @@
-#! /usr/bin/env ts-node
-// I want a script to copy all files from */*/public to public/
-
 import * as fs from "fs";
 import * as path from "path";
 import * as glob from "glob";
 
 const publicDirs = glob.sync("*/**/demo");
 
-publicDirs.forEach((dir) => {
+publicDirs.forEach((dir: string) => {
 	const source = path.resolve(dir);
 	const parent = path.dirname(source).split("/").slice(-1)[0];
 	console.log(parent);
@@ -15,5 +12,8 @@ publicDirs.forEach((dir) => {
 		const dest = path.resolve(process.cwd(), "public", parent);
 		fs.mkdirSync(dest, { recursive: true });
 		fs.cpSync(source, dest, { recursive: true });
+		fs.rmSync(source, { recursive: true });
 	}
 });
+
+fs.cpSync("app/home/src", "public", { recursive: true });
