489adcadc5478f88891ef44f9bcb46a05c3067f9
Jean-Michel FRANCOIS <jeanmichel.francois@veepee.com>
2024-02-03 00:10:36 +0100

    test: add e2e test with playwright

--- app/fuelcalc/src/components/Form.tsx
+++ app/fuelcalc/src/components/Form.tsx
@@ -151,7 +151,7 @@ export const Calc = () => {
 						type="number"
 						className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 						value={format(state.litersTotal)}
-						readOnly
+						name="litersTotal"
 					/>
 				</div>
 				<div className="mb-2 flex flex-col">
