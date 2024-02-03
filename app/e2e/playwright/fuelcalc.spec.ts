5c2c460422f4b65f674bc262885786eba2254966
Jean-Michel FRANCOIS <jeanmichel.francois@gmail.com>
2024-02-03 13:24:35 +0100

    test: add e2e test with playwright (#11)

---
+++ app/e2e/playwright/fuelcalc.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+
+test('fuelcalc', async ({ page }) => {
+	await page.goto('http://localhost:5173')
+	await page.screenshot({ path: './playwright-report/screenshot.png' })
+
+	await page.fill('input[name="laptimeM"]', '2')
+	await page.fill('input[name="laptimeS"]', '0')
+	await page.fill('input[name="raceLength"]', '60')
+	await page.fill('input[name="literPerLap"]', '3')
+	await page.fill('input[name="maxTank"]', '105')
+
+	await expect(page.locator('input[name="litersTotal"]')).toHaveJSProperty(
+		'value',
+		'94'
+	)
+})
