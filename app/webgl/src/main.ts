05b3bc82c0ea10bb820f760d2da682930afca422
Jean-Michel FRANCOIS <jeanmichel.francois@qlik.com>
2025-01-03 17:06:21 +0100

    feat: init

---
+++ app/webgl/src/main.ts
@@ -0,0 +1,24 @@
+function main() {
+	const canvas = document.querySelector('#gl-canvas') as HTMLCanvasElement
+	if (!canvas) {
+		console.error('Unable to find the canvas element with id "glcanvas"')
+		return
+	}
+	// Initialize the GL context
+	const gl = canvas.getContext('webgl')
+
+	// Only continue if WebGL is available and working
+	if (gl === null) {
+		alert(
+			'Unable to initialize WebGL. Your browser or machine may not support it.'
+		)
+		return
+	}
+
+	// Set clear color to black, fully opaque
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	// Clear the color buffer with specified clear color
+	gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+main()
